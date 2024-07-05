98cfeb9483917ffe0f7cd46413e82478196af157
Mithilesh Dharmale <mithilesh12296@gmail.com>
2024-07-05 12:24:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,16 +39,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string=ingredients_string+' '+fruit_chosen
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        #search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
-        st.subheader(fruit_chosen+'  Nutrition Information')
-        #st.write(fruit_chosen + 'Is my FRUIT')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-        #st.write(fruityvice_response + 'Mithilesh')
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        #st.text(fruityvice_response)
-        #st.stop()
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
         
@@ -62,8 +57,6 @@ if ingredients_list:
         values ('""" + ingredients_string+ """','"""+ name_on_order + """')"""
 
     time_to_insert=st.button('Submit Order')
-    #st.write(my_insert_stmt)
-    #st.stop()
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! , '+name_on_order+'', icon="✅")
