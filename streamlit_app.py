57875533ecdb4c3234146ff57b691eb4677cdfea
Mithilesh Dharmale <mithilesh12296@gmail.com>
2024-07-05 12:17:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
         #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         #st.write(fruityvice_response + 'Mithilesh')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-        st.text(fruityvice_response.json())
+        st.text(fruityvice_response)
         st.stop()
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
